b8e33905a5a3a44ac853d57b7362e55f97d01205
anhpi1 <phamhaianh660@gmail.com>
2024-10-30 20:34:30 +0700

    haianh

--- word_list.py
+++ word_list.py
@@ -1,10 +1,5 @@
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+
 from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from sklearn.model_selection import train_test_split
-import numpy as np
 import json
 max_token=10000
 
@@ -13,19 +8,13 @@ def read_sentences_from_file(file_path):
         sentences = file.readlines()  
     return [sentence.strip() for sentence in sentences]  # xóa ký tự newline
 
-
-
 # dữ liệu đầu vào
-x = read_sentences_from_file('x.ta')
-
-
-# dữ liệu đầu ra
-y = read_sentences_from_file('y.ta')
-
+x = read_sentences_from_file('data_train/x.ta')
+print(x)
 # tạo word list
 tokenizer = Tokenizer(num_words=10000, oov_token="<OOV>")
 tokenizer.fit_on_texts(x)
 word_index = tokenizer.word_index
-with open('word_index.json', 'w') as json_file:
+with open('data_train/word_list.json', 'w') as json_file:
     json.dump(word_index, json_file)
 
