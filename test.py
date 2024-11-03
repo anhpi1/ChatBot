0035a3cc331fa977b2c12bfdb12d4e126ffe98cf
anhpi1 <phamhaianh660@gmail.com>
2024-11-04 00:01:27 +0700

    haianh

--- test.py
+++ test.py
@@ -12,11 +12,31 @@ input3 = "What is the third dominant pole in 2 satge comparator?"
 
 
 dst1 = ST.sentencess(input1,dst1)
-print(":{}".format(dst1.Bt))
+print("user 1:{}".format(dst1.Bt))
 dst2 = ST.sentencess(input2,dst2)
 print("user 2:{}".format(dst2.Bt))
 dst3 = ST.sentencess(input3,dst3)
 print("user 3:{}".format(dst3.Bt))
+
+# What is the dominant pole of a Miller compensated two stage comparator?
+# [what]
+# [2 stage comparator]:1
+# [NULL]
+# [Phase margin]1
+# [NULL]
+# [Miller compensation components]
+# [NULL]
+
+8*20 
+
+# [what]
+# [NULL]
+# [Input stager]
+# [Phase margin]
+# [NULL]
+# [Miller compensation components]
+# [NULL]
+
 
 
 
